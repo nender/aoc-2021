8c2e4443154145bf2d9e68c64ec6373699d7bded
Nathan Ender <nender@users.noreply.github.com>
2021-12-02 19:32:58 +0100

    Solve day 2

--- 02/Program.cs
+++ 02/Program.cs
@@ -23,25 +23,53 @@ Direction parseDirection(string Input) =>
         _ => throw new ArgumentException()
     };
 
-var input = GetInput();
-
-var x = 0;
-var y = 0;
-foreach (var order in input) {
-    switch (order.Direction) {
-        case Direction.forward:
-            x += order.Magnitude;
-            break;
-        case Direction.down:
-            y += order.Magnitude;
-            break;
-        case Direction.up:
-            y -= order.Magnitude;
-            break;
+void SolvePartOne(List<Order> input) {
+    var x = 0;
+    var y = 0;
+    foreach (var order in input) {
+        switch (order.Direction) {
+            case Direction.forward:
+                x += order.Magnitude;
+                break;
+            case Direction.down:
+                y += order.Magnitude;
+                break;
+            case Direction.up:
+                y -= order.Magnitude;
+                break;
+        }
     }
+
+    Console.WriteLine($"Final depth: {y}, distance: {x}");
+    Console.WriteLine($"Checksum: {x*y}");
 }
-Console.WriteLine($"Final depth: {y}, distance: {x}");
-Console.WriteLine($"Checksum: {x*y}");
+
+void SolvePartTwo(List<Order> input) {
+    var x = 0;
+    var y = 0;
+    var aim = 0;
+    foreach (var order in input) {
+        switch (order.Direction) {
+            case Direction.forward:
+                x += order.Magnitude;
+                y += aim * order.Magnitude;
+                break;
+            case Direction.down:
+                aim += order.Magnitude;
+                break;
+            case Direction.up:
+                aim -= order.Magnitude;
+                break;
+        }
+    }
+
+    Console.WriteLine($"Final depth: {y}, distance: {x}, aim: {aim}");
+    Console.WriteLine($"Checksum: {x*y}");
+}
+
+var input = GetInput();
+SolvePartOne(input);
+SolvePartTwo(input);
 
 record struct Order(Direction Direction, int Magnitude);
 enum Direction {
