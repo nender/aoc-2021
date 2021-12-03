ca7ce931dbbfb91fd49148c4b87ebf0172cc2f14
Nathan Ender <nender@users.noreply.github.com>
2021-12-03 18:22:43 +0100

    Solve day 3 part 1

---
+++ 03/Program.cs
@@ -0,0 +1,40 @@
+﻿List<ushort> GetInput() {
+    var inputs = new List<ushort>();
+
+    string? input = Console.ReadLine();
+    const int fromBase = 2;
+    while (input != null) {
+        var number = Convert.ToUInt16(input, fromBase);
+        inputs.Add(number);
+        input = Console.ReadLine();
+    }
+
+    return inputs;
+}
+
+const int bitCount = 12;
+var inputs = GetInput();
+var inputCount = inputs.Count;
+
+var bitCounts = new int[bitCount];
+foreach (var reading in inputs) {
+    for (var i = 0; i < bitCount; i++) {
+        var bitIsSet = (reading & 1<<i) != 0;
+        if (bitIsSet) {
+            bitCounts[i] += 1;
+        }
+    }
+}
+
+var gamma = 0;
+var epsilon = 0;
+var threshold = inputCount / 2;
+for (var i = 0; i < bitCounts.Length; i++) {
+    if (bitCounts[i] > threshold) {
+        gamma ^= 1<<i;
+    } else {
+        epsilon ^= 1<<i;
+    }
+}
+
+Console.WriteLine("gamma*epsilon=" + gamma*epsilon);
