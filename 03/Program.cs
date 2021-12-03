bf1e1ee519f492f56d689242d3e155cdc0d422c5
Nathan Ender <nender@users.noreply.github.com>
2021-12-03 19:01:41 +0100

    Kinda stuck on part 2

--- 03/Program.cs
+++ 03/Program.cs
@@ -12,29 +12,64 @@
     return inputs;
 }
 
-const int bitCount = 12;
-var inputs = GetInput();
-var inputCount = inputs.Count;
-
-var bitCounts = new int[bitCount];
-foreach (var reading in inputs) {
-    for (var i = 0; i < bitCount; i++) {
-        var bitIsSet = (reading & 1<<i) != 0;
-        if (bitIsSet) {
-            bitCounts[i] += 1;
+int[] CountBits(IEnumerable<ushort> inputs) {
+    const int bitCount = 12;
+    var bitCounts = new int[bitCount];
+    foreach (var reading in inputs) {
+        for (var i = 0; i < bitCount; i++) {
+            var bitIsSet = (reading & 1<<i) != 0;
+            if (bitIsSet) {
+                bitCounts[i] += 1;
+            }
         }
     }
+
+    return bitCounts;
 }
 
-var gamma = 0;
-var epsilon = 0;
-var threshold = inputCount / 2;
-for (var i = 0; i < bitCounts.Length; i++) {
-    if (bitCounts[i] > threshold) {
-        gamma ^= 1<<i;
-    } else {
-        epsilon ^= 1<<i;
+void SolvePartOne(List<ushort> inputs) {
+    var bitCounts = CountBits(inputs);
+
+    var gamma = 0;
+    var epsilon = 0;
+    var threshold = inputs.Count / 2;
+    for (var i = 0; i < bitCounts.Length; i++) {
+        if (bitCounts[i] > threshold) {
+            gamma ^= 1<<i;
+        } else {
+            epsilon ^= 1<<i;
+        }
     }
+
+    Console.WriteLine("gamma*epsilon=" + gamma*epsilon);
 }
 
-Console.WriteLine("gamma*epsilon=" + gamma*epsilon);
+void SolvePartTwo(List<ushort> inputs) {
+    var candidates = inputs.ToArray();
+    var bitIndex = 0;
+
+    while (candidates.Length > 1) {
+        var bitCount = 0;
+        foreach (var candidate in candidates) {
+            var bitIsSet = (candidate & 1<<bitIndex) != 0;
+            if (bitIsSet) {
+                bitCount += 1;
+            }
+        }
+
+        var threshold = candidates.Length / 2;
+        var filter = bitCount > threshold ? 1 : 0;
+
+        candidates = candidates
+            .Where(x => (x & 1<<bitIndex) == filter)
+            .ToArray();
+
+        bitIndex += 1;
+    }
+
+    var value = candidates[0];
+}
+
+var inputs = GetInput();
+SolvePartOne(inputs);
+SolvePartTwo(inputs);
