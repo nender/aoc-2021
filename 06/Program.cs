0e930a5b4b41a6613241eb7b50274e18c931f2af
Nathan Ender <nender@users.noreply.github.com>
2021-12-06 20:29:01 +0100

    Solve day 6 part one

---
+++ 06/Program.cs
@@ -0,0 +1,23 @@
+﻿IEnumerable<int> GetInput() =>
+    Console.ReadLine()
+        .Split(',')
+        .Select(x => int.Parse(x));
+
+var fish = GetInput().ToList();
+
+foreach (var _ in Enumerable.Range(0, 80)) {
+    var newFish = 0;
+    for (var i = 0; i < fish.Count; i++) {
+        var counter = fish[i];
+        if (counter > 0) {
+            fish[i] = counter - 1;
+        } else {
+            fish[i] = 6;
+            newFish += 1;
+        }
+    }
+
+    fish.AddRange(Enumerable.Repeat(8, newFish));
+}
+
+Console.WriteLine($"After 80 days there are {fish.Count} fish");
