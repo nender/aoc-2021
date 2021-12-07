9ce810fab04053919331c07e3eade2ffe47f93fe
Nathan Ender <nender@users.noreply.github.com>
2021-12-07 18:44:51 +0100

    Solve day 7 part one

---
+++ 07/Program.cs
@@ -0,0 +1,22 @@
+﻿IEnumerable<int> GetInput() =>
+    Console.ReadLine()
+        .Split(',')
+        .Select(x => int.Parse(x));
+
+var crabs = GetInput();
+var minCrab = crabs.Min();
+var maxCrab = crabs.Max();
+
+var minCost = int.MaxValue;
+var minIndex = int.MaxValue;
+for (var i = minCrab; i <= maxCrab; i++) {
+    var cost = crabs.Select(x => Math.Abs(x - i))
+        .Sum();
+    
+    if (cost < minCost) {
+        minCost = cost;
+        minIndex = i;
+    }
+}
+
+Console.WriteLine($"Min cost is: {minCost}");
