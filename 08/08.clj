cf62c87723ed831ab94c5bbdfc628aab1981114d
Nathan Ender <nender@users.noreply.github.com>
2021-12-08 17:54:04 +0100

    Lay out framework for part 2

--- 08/08.clj
+++ 08/08.clj
@@ -11,16 +11,51 @@
          (split-on #(not= "|" %))
          (map #(map set %))))
 
-(defn get-input []
+(def input
     (->> (slurp *in*)
          (str/split-lines)
          (map parse-input)))
 
+(defn decode-signal [[signals digits]]
+    ;; todo implement
+    0000)
+
+(defn descramble-signal [key signal]
+    "abcdefg")
+
+(defn decode-digit [signal]
+    "Given a descrambled signal, return the corresponding digit"
+    (-> {#{\a \b \c \e \f \g} \0
+         #{\c \f} \1
+         #{\a \c \d \e \g} \2
+         #{\a \c \d \f \g} \3
+         #{\b \c \d \f } \4
+         #{\a \b \d \f \g} \5
+         #{\a \b \d \e \f \g} \6
+         #{\a \c \f} \7
+         #{\a \b \c \d \e \f \g} \8
+         #{\a \b \c \d \f \g} \9}
+         (get signal)))
+
 (println
     "Solution to part one:"
-    (->> (get-input)
+    (->> input
          (map second)
          flatten
          (map count)
          (filter #(contains? #{2 4 3 7} %))
-         (count)))
+         count))
+
+(println
+    "Solution to part two:"
+    (->> input
+         (map decode-signal)
+         (reduce +)))
+
+;; Digits and segment counts
+;; 1 - 1
+;; 7 - 3
+;; 2,3,5 - 5
+;; 4 - 4
+;; 0,6,9 - 6
+;; 8 - 7
