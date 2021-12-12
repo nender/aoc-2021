2a1d1d066896130c7e4f5848063fd5bdb23c80d2
Nathan Ender <nender@users.noreply.github.com>
2021-12-12 21:25:18 +0100

    Solve day 12 part one

--- 12/12.clj
+++ 12/12.clj
@@ -18,18 +18,33 @@
        (map #(disj % k))
        (mapcat identity)))
 
-(defn graph-map [pairs]
+(defn graph [pairs]
   (let [all-nodes (set (mapcat identity pairs))]
     (loop [nodes all-nodes
            hash {}]
       (if (empty? nodes)
-        {:nodes all-nodes, :neighbors hash}
+        {:nodes all-nodes, :edges hash}
         (let [node (first nodes)]
           (recur
             (rest nodes)
             (assoc hash node (set (neighbors node pairs)))))))))
 
-(defn uppercase? [str]
-  (not (some #(Character/isLowerCase %) str)))
+(defn lowercase? [str]
+  (not (some #(Character/isUpperCase %) str)))
 
-(pprint (graph-map (read-input "input.txt")))
+(defn walk [graph path to-visit]
+  (let [visited (set path) cur (peek path) neighbors (graph cur)]
+    (if (= "end" cur)
+      [path]
+      (if-let [unvisited (seq (set/difference neighbors (set (filter lowercase? visited))))]
+        (mapcat #(walk graph (conj path %) to-visit) unvisited)))))
+
+(defn valid-paths [{nodes :nodes graph :edges}]
+  (walk graph ["start"] (set (filter lowercase? nodes))))
+
+(defn part-one [input]
+  (->> (graph input)
+       (valid-paths)
+       count))
+
+(println "Part one:" (part-one (read-input "input.txt")))
