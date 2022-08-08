a49833b295c206f7fcbbd11941f577116d456ac9
Nathan Ender <nathan.ender@gmail.com>
2022-08-08 14:25:06 +0200

    Solve day 13 part 2

--- 13/day13.clj
+++ 13/day13.clj
@@ -1,6 +1,5 @@
 (ns day13 
-  (:require [clojure.string :as str]
-            [clojure.set :as set]))
+  (:require [clojure.string :as str]))
 
 (defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
 
@@ -29,35 +28,45 @@
        (split-on (complement empty?))
        parse))
 
-;; generalize code for folding
-(defn fold-y [index height [x y]]
+(defn fold-y [index [x y]]
   (if (< y index)
     [x y]
     [x
-     (- (quot height 2) (- y index))]))
+     (- index (- y index))]))
 
-(defn vertical-fold [dots index]
-  (let [height (apply max (map second dots))]
-    (->> (map #(fold-y index height %) dots)
-         (into #{}))))
-
-(defn fold-x [index height [x y]]
+(defn fold-x [index [x y]]
   (if (< x index)
     [x y]
-    [(- (quot height 2) (- x index))
+    [(- index (- x index))
      y]))
 
-(defn horizontal-fold [dots index]
-  (let [height (apply max (map first dots))]
-    (->> (map #(fold-x index height %) dots)
-         (into #{}))))
+(defn fold-dots [[axis index] dots]
+  (let [folder (case axis
+                 "x" #(fold-x index %)
+                 "y" #(fold-y index %))]
+    (into #{} (map folder dots))))
 
-; only first fold for now
-(defn evaluate-folds [[dots [fold & _]]]
-  (let [[axis index] fold]
-    (case axis
-      "x" (horizontal-fold dots index)
-      "y" (vertical-fold dots index))))
+(defn evaluate-folds [[dots folds]]
+  (loop [dots dots
+         folds folds]
+    (if (empty? folds)
+      dots
+      (recur
+        (fold-dots (first folds) dots)
+        (rest folds)))))
 
-(def testinput (read-input "testinput.txt"))
-(def dots (first testinput))
+(defn print-dots [dots]
+  (let [height (apply max (map second dots))
+        width (apply max (map first dots))]
+    (doseq [y (range (inc height))
+            x (range (inc width))]
+      (if (contains? dots [x y])
+        (print "#")
+        (print " "))
+      (if (= x width)
+        (print "\n")))))
+
+;; exposing the solution to part one again is left as an excercise to the
+;reader.
+(println "Part two:")
+(print-dots (evaluate-folds (read-input "input.txt")))
